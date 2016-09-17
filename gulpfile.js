451d584d73b31d413bed1d2fff813c15add25f2b
desmondyao <yaojy1121@gmail.com>
2016-09-17 22:50:29 +0800

    Update by gulp script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('gh', shell.task([
+gulp.task('git', shell.task([
   'git add . ',
   'git commit . -m \"Update by gulp script.\"',
   'git push origin master --force' 
 ]));
 
+gulp.task('s', shell.task([
+	'sass source/css/site.scss:source/css/site.css'
+]));
+
 gulp.task('sw', shell.task([
 	'sass -w source/css/site.scss:source/css/site.css'
 ]));
+
+gulp.task('upload', ['git', 's']);
