07ba637b6f2a2e2f6b67f20dcdda7074ee8bab11
desmondyao <yaojy1121@gmail.com>
2016-09-17 23:16:51 +0800

    Update by gulp script.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,22 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var git = require('gulp-git');
 
-gulp.task('git', function() {
+gulp.task('git:add', function() {
 	return gulp.src('./')
-		.pipe(git.add())
-		.pipe(git.commit('Upload by gulp script.'))
-		.pipe(git.push('origin', 'master', {args: " -f"}, function (err) {
-	    if (err) throw err;
-	  }));
+		.pipe(git.add());
 });
+
+gulp.task('git:commit', function() {
+	return gulp.src('./')
+		.pipe(git.commit('Update by gulp script.'))
+		.on('end', function() {
+			git.push('origin', 'master', {args: " -f"}, function (err) {
+		    if (err) throw err;
+		  });
+		});
+});
+
+gulp.task('git', ['git:add', 'git:commit']);
 
 gulp.task('s', function () {
   return gulp.src('source/css/site.scss')
