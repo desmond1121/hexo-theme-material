36e1e94f44749df6bb2cbd36624cd509bf4380e7
desmondyao <yaojy1121@gmail.com>
2016-09-17 23:08:55 +0800

    Upload by gulp script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var shell = require('gulp-shell');
+var git = require('gulp-git');
 
-gulp.task('git', shell.task([
-  'git add . ',
-  'git commit . -m \"Update by gulp script.\"',
-  'git push origin master --force' 
-]));
+gulp.task('git', function() {
+	return gulp.src('./')
+		.pipe(git.add())
+		.pipe(git.commit('Upload by gulp script.'))
+		.pipe(git.push('origin', 'master', {args: " -f"}, function (err) {
+	    if (err) throw err;
+	  }));
+});
 
 gulp.task('s', function () {
   return gulp.src('source/css/site.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('source/css'));
 });
-
-gulp.task('sw', shell.task([
-	'sass -w source/css/site.scss:source/css/site.css'
-]));
 
 gulp.task('upload', ['s', 'git']);
