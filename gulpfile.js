d1f2afb5ac643e1340c0a5c17d7e4d145311ad2d
desmondyao <yaojy1121@gmail.com>
2016-10-07 21:22:12 +0800

    Update by gulp script.

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var git = require('gulp-git');
 
-gulp.task('git:add', function() {
-	return gulp.src('./')
-		.pipe(git.add());
-});
-
-gulp.task('git:commit', function() {
-	return gulp.src('./')
-		.pipe(git.commit('Update by gulp script.'))
-		.on('end', function() {
-			git.push('origin', 'master', {args: " -f"}, function (err) {
-		    if (err) throw err;
-		  });
-		});
-});
-
-gulp.task('git', ['git:add', 'git:commit']);
-
 gulp.task('s', function () {
   return gulp.src('source/css/site.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('source/css'));
 });
 
-gulp.task('upload', ['s', 'git']);
+
+gulp.task('git:add', ['s'], function() {
+	return gulp.src('./')
+		.pipe(git.add());
+});
+
+gulp.task('git:commit', ['git:add'], function() {
+	return gulp.src('./')
+		.pipe(git.commit('Update by gulp script.'));
+});
+
+gulp.task('git:push', function(){
+  git.push('origin', 'master', function (err) {
+    if (err) throw err;
+  });
+});
+
+gulp.task('default', ['git:push']);
